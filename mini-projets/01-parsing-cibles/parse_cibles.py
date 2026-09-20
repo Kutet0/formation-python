4d2fc86df7bd654bc1eeb5241755e08dd333cff4
Kutet0 <ednirrochalima.pro@gmail.com>
2026-09-20 21:59:23 +0200

    Correction premier exercice de parsing

--- mini-projets/01-parsing-cibles/parse_cibles.py
+++ mini-projets/01-parsing-cibles/parse_cibles.py
@@ -1,11 +1,11 @@
-liste_cibles = []
-with open("cibles.txt", encoding="utf-8") as f:
-    for ligne in f:
-        morceaux = ligne.strip().split(":")
-        ip = morceaux[0]
-        port = int(morceaux[1])
-        cible = {"IP": ip, "PORT": port}
-        liste_cibles.append(cible)
+def ligne_vers_cible(ligne):
+    ip, port = ligne.strip().split(":")
+    cible = {"IP": ip, "PORT": int(port)}
+    return cible
 
-for cible in liste_cibles:
-    print(f"{cible["IP"]} -> {cible["PORT"]}")
+
+with open("cibles.txt", encoding="utf-8") as f:
+    cibles = [ligne_vers_cible(l) for l in f]
+
+for cible in cibles:
+    print(f"{cible['IP']} -> {cible['PORT']}")
