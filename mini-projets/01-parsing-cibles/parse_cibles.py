6f8e79239e9878a03c84da22cd7c4fae54ab58a6
Kutet0 <ednirrochalima.pro@gmail.com>
2026-09-20 22:55:58 +0200

    Deuxieme correction premier exercice de parsing

--- mini-projets/01-parsing-cibles/parse_cibles.py
+++ mini-projets/01-parsing-cibles/parse_cibles.py
@@ -3,9 +3,12 @@ def ligne_vers_cible(ligne):
     cible = {"IP": ip, "PORT": int(port)}
     return cible
 
-
 with open("cibles.txt", encoding="utf-8") as f:
-    cibles = [ligne_vers_cible(l) for l in f]
+    cibles = [ligne_vers_cible(l) for l in f if l.strip() and not l.strip().startswith("#")]
+
+ips = {c["IP"] for c in cibles}
+count = {ip : sum(1 for c in cibles if c["IP"] == ip) for ip in ips}
+print(count)
 
 for cible in cibles:
     print(f"{cible['IP']} -> {cible['PORT']}")
