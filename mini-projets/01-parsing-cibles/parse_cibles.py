15fee2e245a7d397a60534013d536dffccdb64fc
Kutet0 <ednirrochalima.pro@gmail.com>
2026-09-21 00:54:59 +0200

    Deuxieme exercice de construction de commande

--- mini-projets/01-parsing-cibles/parse_cibles.py
+++ mini-projets/01-parsing-cibles/parse_cibles.py
@@ -3,8 +3,8 @@ def ligne_vers_cible(ligne):
     cible = {"IP": ip, "PORT": int(port)}
     return cible
 
-with open("cibles.txt", encoding="utf-8") as f:
-    cibles = [ligne_vers_cible(l) for l in f if l.strip() and not l.strip().startswith("#")]
+with open("cibles.txt", encoding="utf-8") as file:
+    cibles = [ligne_vers_cible(ligne) for ligne in file if ligne.strip() and not ligne.strip().startswith("#")]
 
 ips = {c["IP"] for c in cibles}
 count = {ip : sum(1 for c in cibles if c["IP"] == ip) for ip in ips}
